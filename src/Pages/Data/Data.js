d4d65b875edb1a9b1eba873ece32a3664168607a
saddaul-siam <saddadul.siam@gmail.com>
2021-11-28 16:48:27 +0600

    added myproject section

---
+++ src/Pages/Data/Data.js
@@ -0,0 +1,83 @@
+const data = [
+  {
+    name: 'Food Network',
+    img: "https://i.ibb.co/SwtGj3b/project1.png",
+    html: 'HTML',
+    css: 'CSS',
+  },
+  {
+    name: 'Honda CBR',
+    img: "https://i.ibb.co/8PhD1T3/project2.png",
+    html: 'HTML',
+    css: 'CSS',
+    bootstrap: 'Bootstrap'
+  },
+  {
+    name: 'Responsive Football',
+    img: "https://i.ibb.co/5ThXVCY/Untitled-2.png",
+    html: 'HTML',
+    css: 'CSS',
+    bootstrap: 'Bootstrap'
+  },
+  {
+    name: 'Siam Store (e-commerce Website)',
+    img: "https://i.ibb.co/WzK9HqM/project5.png",
+    html: 'HTML',
+    css: 'CSS',
+    bootstrap: 'Bootstrap',
+    javascript: 'Javascript',
+  },
+  {
+    name: 'Easy School (educational Website)',
+    img: "https://i.ibb.co/XSN7cJz/project8.png",
+    react: 'React',
+    bootstrap: 'Bootstrap',
+  },
+  {
+    name: 'Pro medical (healthcare-website.)',
+    img: "https://i.ibb.co/VthncpG/project7.png",
+    react: 'React',
+    bootstrap: 'Bootstrap',
+    router: 'React Router',
+  },
+  {
+    name: 'Tour Planners (Tourism Website)',
+    img: "https://i.ibb.co/ChJDNkv/project9.png",
+    react: 'React',
+    router: 'React Router',
+    bootstrap: 'Bootstrap',
+    firebase: 'Firebase',
+    node: 'Node',
+    mongoDb: 'MongoDB'
+  },
+  {
+    name: 'Bike Buzz (e-commerce Website)',
+    img: "https://i.ibb.co/QMh2KVs/project6.png",
+    react: 'React',
+    router: 'React Router',
+    materialUI: 'MaterialUI',
+    firebase: 'Firebase',
+    node: 'Node',
+    mongoDb: 'MongoDB'
+  },
+  {
+    name: 'Doctors Portal',
+    img: "https://i.ibb.co/LvD14fj/projects10.png",
+    react: 'React',
+    router: 'React Router',
+    materialUI: 'MaterialUI',
+    firebase: 'Firebase',
+    node: 'Node',
+    mongoDb: 'MongoDB'
+  },
+  {
+    name: 'Jerins Parlour',
+    img: "https://i.ibb.co/XLHCYMD/project11.png",
+    react: 'React',
+    router: 'React Router',
+    materialUI: 'MaterialUI',
+    firebase: 'Firebase',
+    node: 'Node',
+    mongoDb: 'MongoDB'
+  },
+]
