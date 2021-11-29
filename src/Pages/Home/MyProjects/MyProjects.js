4d252d28422808d283fde92c3090a090e277d1b5
saddaul-siam <saddadul.siam@gmail.com>
2021-11-29 21:52:05 +0600

    added projects deteils

--- src/Pages/Home/MyProjects/MyProjects.js
+++ src/Pages/Home/MyProjects/MyProjects.js
@@ -38,7 +38,7 @@ const MyProjects = () => {
           "modifier": 1,
           "slideShadows": true
         }} pagination={true} className="mySwiper">
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/SwtGj3b/project1.png" alt="" />
             <Box sx={{ pt: 3 }}>
@@ -53,11 +53,12 @@ const MyProjects = () => {
               <a target="_blank" href="https://github.com/Saddaul-Siam/food-network" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Code</Button></a>
             </Box>
           </Card>
-        </SwiperSlide>
+        </SwiperSlide> */}
         <SwiperSlide>
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/8PhD1T3/project2.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "18px" }}>Technology</Typography>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">HTML</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">CSS</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="success">Bootstrap</Button>
@@ -65,6 +66,15 @@ const MyProjects = () => {
             <Typography variant="h5" sx={{ m: 2 }}>
               Honda CBR
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p" >
+                This is a single-page website. (only client site)
+                <br />
+                This is a bike-selling website.
+                <br />
+                e-commerce Website
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://honda-cbr-24.netlify.app/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/honda-cbr" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Code</Button></a>
@@ -75,6 +85,7 @@ const MyProjects = () => {
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/5ThXVCY/Untitled-2.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "20px" }}>Technology</Typography>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">HTML</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">CSS</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="success">Bootstrap</Button>
@@ -82,6 +93,15 @@ const MyProjects = () => {
             <Typography variant="h5" sx={{ m: 2 }}>
               Responsive Football Website
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p" >
+                This is a single-page website. (only client site)
+                <br />
+                Football news sharing website
+                <br />
+
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://saddaul-siam.github.io/responsive-football/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/responsive-football" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Code</Button></a>
@@ -92,6 +112,7 @@ const MyProjects = () => {
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/WzK9HqM/project5.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "20px" }}>Technology</Typography>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">HTML</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">CSS</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="success">Bootstrap</Button>
@@ -100,6 +121,15 @@ const MyProjects = () => {
             <Typography variant="h5" sx={{ m: 2 }}>
               Siam Store (e-commerce Website)
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p" >
+                This is a single-page website. (only client site)
+                <br />
+                Simple javascript website
+                <br />
+                e-commerce Website
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://siam-store.netlify.app/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/siam-store" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Code</Button></a>
@@ -110,12 +140,22 @@ const MyProjects = () => {
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/XSN7cJz/project8.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "20px" }}>Technology</Typography>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">React</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">Bootstrap</Button>
             </Box>
             <Typography variant="h5" sx={{ m: 2 }}>
               Easy School (educational Website)
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p" >
+                This is a single-page website. (only client site)
+                <br />
+                Simple React website
+                <br />
+                Online learning website
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://online-easy-school.netlify.app/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/Educational-services" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Code</Button></a>
@@ -126,13 +166,24 @@ const MyProjects = () => {
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/VthncpG/project7.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "20px" }}>Technology</Typography>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">React</Button>
-              <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">Bootstrap</Button>
+              <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="success">Bootstrap</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">React Router</Button>
+              <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="warning">Firebase</Button>
             </Box>
             <Typography variant="h5" sx={{ m: 2 }}>
               Pro medical (healthcare-website.)
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p" >
+                This is a single-page website. (only client site)
+                <br />
+                Pro medical is online hospital
+                <br />
+                Users need to log in via Google (also can create a new account)
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://pro-medical-24.web.app/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/Pro-Medical" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Code</Button></a>
@@ -143,6 +194,7 @@ const MyProjects = () => {
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/ChJDNkv/project9.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "20px" }}>Technology</Typography>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">React</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">React Router</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="success">Bootstrap</Button>
@@ -153,6 +205,17 @@ const MyProjects = () => {
             <Typography variant="h5" sx={{ m: 2 }}>
               Tour Planners (Tourism Website)
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p" >
+                This is a full-stack Tour-Planner booking website.
+                <br />
+                Users can be booking tours
+                <br />
+                Users need to log in via Google (also can create a new account)
+                <br />
+                Have admin and user dashboard. Admin can delete, add, manage services.
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://roma-tourism.web.app/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/Tour-Planners-client" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Client Site Code</Button></a>
@@ -164,6 +227,7 @@ const MyProjects = () => {
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/QMh2KVs/project6.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "20px" }}>Technology</Typography>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">React</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">React Router</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="success">MaterialUI</Button>
@@ -174,6 +238,17 @@ const MyProjects = () => {
             <Typography variant="h5" sx={{ m: 2 }}>
               Bike Buzz (e-commerce Website)
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p">
+                This is a full-stack bike-selling website.
+                <br />
+                Users can order bikes and need to cancel orders
+                <br />
+                Users need to log in via Google (also can create a new account)
+                <br />
+                Have admin and user dashboard. Admin can delete, add, manage services.
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://bike-buzz.web.app/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/Bike-Buzz-Client" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Client Site Code</Button></a>
@@ -185,6 +260,7 @@ const MyProjects = () => {
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/LvD14fj/projects10.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "20px" }}>Technology</Typography>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">React</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">React Router</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="success">MaterialUI</Button>
@@ -195,6 +271,17 @@ const MyProjects = () => {
             <Typography variant="h5" sx={{ m: 2 }}>
               Doctors Portal
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p">
+                This is a full-stack Doctors website.
+                <br />
+                Users can booking a Appointments
+                <br />
+                Users need to log in via Google (also can create a new account)
+                <br />
+                Have admin and user dashboard. Admin can delete, add, manage services.
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://doctors-portal-24.web.app/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/Doctor-protal-client" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Client Site Code</Button></a>
@@ -206,6 +293,7 @@ const MyProjects = () => {
           <Card style={{ height: "100%", padding: 20, borderRadius: "20px" }}>
             <img src="https://i.ibb.co/XLHCYMD/project11.png" alt="" />
             <Box sx={{ pt: 3 }}>
+              <Typography variant="span" sx={{ fontSize: "20px" }}>Technology</Typography>
               <Button size="small" sx={{ ml: 2, borderRadius: '52px', cursor: "default" }} variant="outlined" color="error">React</Button>
               <Button size="small" sx={{ mx: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="info">React Router</Button>
               <Button size="small" sx={{ mr: 1, borderRadius: '52px', cursor: "default" }} variant="outlined" color="success">MaterialUI</Button>
@@ -216,6 +304,17 @@ const MyProjects = () => {
             <Typography variant="h5" sx={{ m: 2 }}>
               Jerins Parlour
             </Typography>
+            <Box sx={{ m: 2 }}>
+              <Typography variant="p">
+                This is a full-stack Parlour website.
+                <br />
+                Users need to booking a services and cancel services
+                <br />
+                Users can log in via Google (also can create a new account)
+                <br />
+                Have admin and user dashboard. Admin can delete, add, manage services.
+              </Typography>
+            </Box>
             <Box>
               <a target="_blank" href="https://jerins-parlours.web.app/" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ ml: 2, borderRadius: '52px', }} variant="contained" color="warning">Live Site</Button></a>
               <a target="_blank" href="https://github.com/Saddaul-Siam/Jerins-parlour-client" rel="noReferrer" style={{ textDecoration: 'none' }}><Button sx={{ m: 1, borderRadius: '52px', }} variant="contained" color="primary">Client Site Code</Button></a>
